b4f21960498631731162f525fcd53986d24ed83a
atishayj281 <atishay.tca1909005@tmu.ac.in>
2021-07-18 10:54:25 +0530

    Error message resolved

--- script.js
+++ script.js
@@ -25,11 +25,13 @@ function getData () {
 
 function displayData (response) {
     // console.log(response);
+    const error = document.querySelector(".error");
     if (response.cod === "404") {
-        const error = document.querySelector(".error");
+        
         error.textContent = "Please enter a valid city";
         search.value = "";
     } else {
+        error.textContent = "";
         const city = document.querySelector(".city");
         city.innerText = `${response.name}, ${response.sys.country}`;
 
